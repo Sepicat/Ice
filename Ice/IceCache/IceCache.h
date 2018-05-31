458148ed616b162b8a532965f33025e582628a54
Harry Duan <harry.duan@bytedance.com>
2018-05-31 09:53:57 +0800

    Initial the IceCache Proj
    [Feature] 初始化项目，建立分支，建立 Playground
    
    Signed-off-by: Harry Duan <gua@desgard.com>

---
+++ Ice/IceCache/IceCache.h
@@ -0,0 +1,19 @@
+//
+//  IceCache.h
+//  IceCache
+//
+//  Created by Gua on 2018/5/31.
+//  Copyright © 2018 Gua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IceCache.
+FOUNDATION_EXPORT double IceCacheVersionNumber;
+
+//! Project version string for IceCache.
+FOUNDATION_EXPORT const unsigned char IceCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IceCache/PublicHeader.h>
+
+
