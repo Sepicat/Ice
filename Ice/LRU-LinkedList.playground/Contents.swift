22fc3778eb180591dbcf933767aa5adafd1d6ded
Desgard_Duan <gua@desgard.com>
2018-06-01 08:00:28 +0800

    完成 LRU 基本操作
    [Feature] 完成 LinkedList 基本操作
    [Feature] 完成 LRU Cache 基本操作
    
    Signed-off-by: Desgard_Duan <gua@desgard.com>

--- Ice/LRU-LinkedList.playground/Contents.swift
+++ Ice/LRU-LinkedList.playground/Contents.swift
@@ -73,6 +73,112 @@ public extension LinkedList {
     /// - Parameter index: Integer value of the node's index to be returned
     /// - Returns: LinkedListNode? => Node?
     public func node(at index: Int) -> Node? {
+        if index < count {
+            var iterativeTime = index
+            var node = head
+            while iterativeTime != 0 && node != nil {
+                iterativeTime -= 1
+                node = head?.next
+            }
+            return node
+        }
         return nil
+    }
+    
+    /// Append a value to the end of the list auto transform a Node object
+    ///
+    /// - Parameter value: The data value to be appended
+    public func append(_ value: ValueType) {
+        let newNode = Node(value: value)
+        append(newNode)
+    }
+    
+    /// Append a copy of a LinkedListNode to the end of the list.
+    ///
+    /// - Parameter node: The node containing the value to be appended
+    public func append(_ node: Node) {
+        let newNode = node
+        if let lastNode = tail {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+        count += 1
+    }
+    
+    public func insert(_ newNode: Node, at index: Int) {
+        if index == 0 {
+            newNode.next = head
+            head?.previous = newNode
+            head = newNode
+        } else {
+            let prev = node(at: index - 1)
+            let next = prev?.next
+            newNode.previous = prev
+            newNode.next = next
+            next?.previous = newNode
+            prev?.next = newNode
+        }
+        count += 1
+    }
+    
+    @discardableResult public func remove(node: Node) -> ValueType {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        
+        node.previous = nil
+        node.next = nil
+        
+        count -= 1
+        return node.value
+    }
+}
+
+public class LRUCache<KeyType: Hashable, ValueType: AnyObject> {
+    
+    typealias Node = LinkedList<ValueType>.LinkedListNode<ValueType>
+    
+    private(set) var capacity: Int
+    
+    private var priority: LinkedList<ValueType> = LinkedList<ValueType>()
+
+    private var hashMap: [KeyType: Node] = [:]
+    
+    public init(_ capacity: Int) {
+        self.capacity = capacity
+    }
+    
+    public func get(_ key: KeyType) -> ValueType? {
+        guard let node = hashMap[key] else {
+            return nil
+        }
+        priority.remove(node: node)
+        priority.insert(node, at: 0)
+        return node.value
+    }
+    
+    public func set(_ key: KeyType, value: ValueType) {
+        if let node = hashMap[key] {
+            node.value = value
+            return
+        }
+        let newNode = Node(value: value)
+        priority.insert(newNode, at: 0)
+        hashMap[key] = newNode
+        
+        while priority.count > capacity {
+            guard let tailNode = priority.tail else {
+                return
+            }
+            priority.remove(node: tailNode)
+        }
     }
 }
